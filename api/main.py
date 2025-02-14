f38929040e80be2142df54a7c81d1f698e86d96b
Bumi <ebumi@agh.edu.pl>
2025-02-14 22:34:25 +0100

    main

--- api/main.py
+++ api/main.py
@@ -1,50 +1,44 @@
-from typing import List
-
 from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from typing import List
 
-import schemas
 import models
+import schemas
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="../ui/build/static", check_dir=False), name="static")
-
 
 @app.get("/")
 def serve_react_app():
     return FileResponse("../ui/build/index.html")
 
-
-@app.get("/movies", response_model=List[schemas.Movie])
+@app.get("/movies/", response_model=List[schemas.Movie])
 def get_movies():
     return list(models.Movie.select())
+@app.get("/movies/{movie_id}", response_model=schemas.Movie)
+def get_movie(movie_id: int):
+    movie = models.Movie.filter(models.Movie.id == movie_id).first()
+    if movie is None:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    return movie
 
-
-@app.post("/movies", response_model=schemas.Movie)
+@app.post("/movies/", response_model=schemas.Movie)
 def add_movie(movie: schemas.MovieBase):
-    movie = models.Movie.create(**movie.dict())
+    movie = models.Movie.create(**movie.model_dump())
     return movie
 
 
-@app.get("/movies/{movie_id}", response_model=schemas.Movie)
-def get_movie(movie_id: int):
-    db_movie = models.Movie.filter(models.Movie.id == movie_id).first()
-    if db_movie is None:
-        raise HTTPException(status_code=404, detail="Movie not found")
-    return db_movie
-
-
 @app.delete("/movies/{movie_id}", response_model=schemas.Movie)
-def get_movie(movie_id: int):
-    db_movie = models.Movie.filter(models.Movie.id == movie_id).first()
-    if db_movie is None:
+def delete_movie(movie_id: int):
+    movie = models.Movie.filter(models.Movie.id == movie_id).first()
+    if movie is None:
         raise HTTPException(status_code=404, detail="Movie not found")
-    db_movie.delete_instance()
-    return db_movie
+    movie.delete_instance()
+    return movie
 
-#added actors:
-@app.get("/actors", response_model=List[schemas.Actor])
+#actors:
+@app.get("/actors/", response_model=List[schemas.Actor])
 def get_actors():
     return list(models.Actor.select())
 
@@ -55,9 +49,9 @@ def get_actor(actor_id: int):
         raise HTTPException(status_code=404, detail="Actor not found")
     return actor
 
-@app.post("/actors", response_model=schemas.Actor)
+@app.post("/actors/", response_model=schemas.Actor)
 def add_actor(actor: schemas.ActorBase):
-    newActor = models.Actor.create(**actor.dict())
+    newActor = models.Actor.create(**actor.model_dump())
     return newActor
 
 @app.delete("/actors/{actor_id}", response_model=schemas.Actor)
